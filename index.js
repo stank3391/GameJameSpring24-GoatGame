ef450ac41a6e8bc821be459b2fb0bd988ade2505
Justin Schulz <jschulz8138@conestogac.on.ca>
2024-02-26 17:52:17 -0500

    created event listener and started animation

--- index.js
+++ index.js
@@ -17,5 +17,53 @@ backgroundImage.onload = () => {
     const scale = canvas.width / backgroundImage.width;
     console.log (scale)
     c.drawImage(backgroundImage, 0, 0, backgroundImage.width * scale, backgroundImage.height * scale)
-    c.drawImage(playerImage, 0, 0, playerImage.width / 4, playerImage.height / 5, 0, 0 , (playerImage.width /4) * 2.2, (playerImage.height /5) * 2.2)
+    c.drawImage(playerImage, 0, 0, playerImage.width / 4, playerImage.height / 5, 500, 500 , (playerImage.width /4) * 2.2, (playerImage.height /5) * 2.2)
 }
+
+
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = positon
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(image, 0, 0, backgroundImage.width * scale, backgroundImage.height * scale)
+
+    }
+}
+const background = new Sprite({ postion: {x: 0, y: 0}, image: image })
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    background.draw()
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height / 5,
+        500,
+        500,
+        (playerImage.width / 4) * 2.2,
+        (playerImage.height / 5) * 2.2)
+}
+
+animate()
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            console.log('pressed w')
+            break;
+        case 'a':
+            console.log('pressed a')
+            break;
+        case 's':
+            console.log('pressed s')
+            break;
+        case 'd':
+            console.log('pressed d')
+            break;
+    }
+})
